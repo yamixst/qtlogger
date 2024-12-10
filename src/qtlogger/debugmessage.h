fd5372a05ac5c4da6b0d435089d70f3006b8146a
Mikhail Yatsenko <mikhail.yatsenko@gmail.com>
2024-12-10 20:39:13 +0300

    Refactor DebugMessage class: remove implementation file, enhance constructor, and add metadata handling

--- src/qtlogger/debugmessage.h
+++ src/qtlogger/debugmessage.h
@@ -4,8 +4,7 @@
 #pragma once
 
 #include <QDateTime>
-#include <QMetaType>
-#include <QString>
+#include <QHash>
 #include <QThread>
 #include <qlogging.h>
 
@@ -16,9 +15,29 @@ namespace QtLogger {
 class QTLOGGER_EXPORT DebugMessage
 {
 public:
-    DebugMessage() = default;
-    DebugMessage(QtMsgType type, const QMessageLogContext &context, const QString &message);
-    DebugMessage(const DebugMessage &dmesg);
+    constexpr DebugMessage() noexcept = default;
+
+    DebugMessage(QtMsgType type, const QMessageLogContext &context, const QString &message) noexcept
+        : m_type(type),
+          m_context(context.file, context.line, context.function, context.category),
+          m_message(message)
+    {
+    }
+
+    DebugMessage(const DebugMessage &dmesg) noexcept
+        : m_file(dmesg.m_context.file),
+          m_function(dmesg.m_context.function),
+          m_category(dmesg.m_context.category),
+          m_type(dmesg.m_type),
+          m_context(m_file.constData(), dmesg.m_context.line, m_function.constData(),
+                    m_category.constData()),
+          m_message(dmesg.m_message),
+          m_time(dmesg.m_time),
+          m_threadId(dmesg.m_threadId),
+          m_formattedMessage(dmesg.m_formattedMessage),
+          m_metadata(dmesg.m_metadata)
+    {
+    }
 
     QtMsgType type() const;
     const QMessageLogContext &context() const;
@@ -30,26 +49,31 @@ public:
     const char *category() const;
 
     inline QDateTime time() const { return m_time; }
-    inline qint64 threadId() const { return reinterpret_cast<qint64>(m_threadId); }
+    inline qintptr threadId() const { return reinterpret_cast<qintptr>(m_threadId); }
 
     QString formattedMessage() const;
     void setFormattedMessage(const QString &formattedMessage);
     bool isFormatted() const;
 
+    QVariant metadata(const QByteArray &name) const;
+    void setMetadata(const QByteArray &name, const QVariant &value);
+    bool hasMetadata(const QByteArray &name) const;
+
 private:
-    QtMsgType m_type = QtDebugMsg;
-    QMessageLogContext m_context;
-    QString m_message;
-
     // context buffers
-    QByteArray m_file;
-    QByteArray m_function;
-    QByteArray m_category;
+    const QByteArray m_file;
+    const QByteArray m_function;
+    const QByteArray m_category;
 
-    QDateTime m_time = QDateTime::currentDateTime();
-    Qt::HANDLE m_threadId = QThread::currentThreadId();
+    const QtMsgType m_type = QtDebugMsg;
+    const QMessageLogContext m_context;
+    const QString m_message;
+
+    const QDateTime m_time = QDateTime::currentDateTime();
+    const Qt::HANDLE m_threadId = QThread::currentThreadId();
 
     QString m_formattedMessage;
+    QHash<QByteArray, QVariant> m_metadata;
 };
 
 inline QtMsgType DebugMessage::type() const
@@ -102,4 +126,19 @@ inline bool DebugMessage::isFormatted() const
     return !m_formattedMessage.isNull();
 }
 
+inline QVariant DebugMessage::metadata(const QByteArray &name) const
+{
+    return m_metadata.value(name);
 }
+
+inline void DebugMessage::setMetadata(const QByteArray &name, const QVariant &value)
+{
+    m_metadata.insert(name, value);
+}
+
+inline bool DebugMessage::hasMetadata(const QByteArray &name) const
+{
+    return m_metadata.contains(name);
+}
+
+} // namespace QtLogger
