849cca664f1949dd42763b857fc6e560189339e4
Mikhail Yatsenko <mikhail.yatsenko@gmail.com>
2024-12-27 22:35:07 +0300

    Introduce TypedPipeline class and refactor Logger to inherit from it

---
+++ src/qtlogger/typedpipeline.cpp
@@ -0,0 +1,190 @@
+#include "typedpipeline.h"
+
+namespace QtLogger {
+
+TypedPipeline::TypedPipeline() { }
+
+QTLOGGER_DECL_SPEC
+void TypedPipeline::insertAfter(HandlerType type, const HandlerPtr &handler)
+{
+    auto first = std::find_if(handlers().begin(), handlers().end(),
+                              [type](const HandlerPtr &x) { return x->type() == type; });
+
+    if (first == handlers().end()) {
+        handlers().prepend(handler);
+        return;
+    }
+
+    auto last = std::find_if(first, handlers().end(),
+                             [type](const HandlerPtr &x) { return x->type() != type; });
+
+    handlers().insert(last, handler);
+}
+
+QTLOGGER_DECL_SPEC
+void TypedPipeline::insertBetween(HandlerType leftType, HandlerType rightType,
+                                  const HandlerPtr &handler)
+{
+    auto firstLeft = std::find_if(handlers().begin(), handlers().end(),
+                              [leftType](const HandlerPtr &x) { return x->type() == leftType; });
+
+    if (firstLeft == handlers().end()) {
+
+        auto firstRight =
+                std::find_if(handlers().begin(), handlers().end(),
+                             [rightType](const auto &x) { return x->type() == rightType; });
+
+        if (firstRight != handlers().end()) {
+            handlers().insert(firstRight, handler);
+        } else {
+            handlers().prepend(handler);
+        }
+
+        return;
+    }
+
+    auto lastLeft = std::find_if(firstLeft, handlers().end(),
+                             [leftType](const HandlerPtr &x) { return x->type() != leftType; });
+
+    handlers().insert(lastLeft, handler);
+}
+
+QTLOGGER_DECL_SPEC
+void TypedPipeline::clearType(HandlerType type)
+{
+    QMutableListIterator<HandlerPtr> iter(handlers());
+
+    while (iter.hasNext()) {
+        if (iter.next()->type() == type) {
+            iter.remove();
+        }
+    }
+}
+
+QTLOGGER_DECL_SPEC
+void TypedPipeline::appendFilter(const FilterPtr &filter)
+{
+    if (filter.isNull())
+        return;
+
+    auto index = std::find_if(handlers().begin(), handlers().end(),
+                              [](const auto &x) { return x->type() != HandlerType::Filter; });
+
+    handlers().insert(index, filter);
+}
+
+QTLOGGER_DECL_SPEC
+FunctionFilterPtr
+TypedPipeline::appendFilter(const std::function<bool(const LogMessage &)> &function)
+{
+    const auto f = FunctionFilterPtr::create(function);
+
+    appendFilter(f);
+
+    return f;
+}
+
+QTLOGGER_DECL_SPEC
+RegExpFilterPtr TypedPipeline::appendFilter(const QRegularExpression &regExp)
+{
+    const auto f = RegExpFilterPtr::create(regExp);
+
+    appendFilter(f);
+
+    return f;
+}
+
+QTLOGGER_DECL_SPEC
+void TypedPipeline::clearFilters()
+{
+    clearType(HandlerType::Filter);
+}
+
+QTLOGGER_DECL_SPEC
+void TypedPipeline::setFormatter(const FormatterPtr &formatter)
+{
+    if (formatter.isNull())
+        return;
+
+    clearFormatters();
+
+    auto index = std::find_if(handlers().begin(), handlers().end(),
+                              [](const auto &x) { return x->type() != HandlerType::Filter; });
+
+    handlers().insert(index, formatter);
+}
+
+QTLOGGER_DECL_SPEC
+FunctionFormatterPtr TypedPipeline::setFormatter(const FunctionFormatter::Function &function)
+{
+    const auto f = FunctionFormatterPtr::create(function);
+
+    setFormatter(f);
+
+    return f;
+}
+
+QTLOGGER_DECL_SPEC
+PatternFormatterPtr TypedPipeline::setFormatter(const QString &pattern)
+{
+    const auto f = PatternFormatterPtr::create(pattern);
+
+    setFormatter(f);
+
+    return f;
+}
+
+QTLOGGER_DECL_SPEC
+void TypedPipeline::clearFormatters()
+{
+    clearType(HandlerType::Formatter);
+}
+
+QTLOGGER_DECL_SPEC
+void TypedPipeline::appendSink(const SinkPtr &sink)
+{
+    append(sink);
+}
+
+QTLOGGER_DECL_SPEC
+void TypedPipeline::clearSinks()
+{
+    clearType(HandlerType::Sink);
+}
+
+QTLOGGER_DECL_SPEC
+void TypedPipeline::appendPipeline(const PipelinePtr &pipeline)
+{
+    append(pipeline);
+}
+
+QTLOGGER_DECL_SPEC
+void TypedPipeline::clearPipelines()
+{
+    clearType(HandlerType::Pipeline);
+}
+
+QTLOGGER_DECL_SPEC
+void TypedPipeline::flush()
+{
+    for (auto &handler : handlers()) {
+        switch (handler->type()) {
+        case HandlerType::Sink: {
+            auto sink = handler.dynamicCast<Sink>();
+            if (sink)
+                sink->flush();
+            break;
+        }
+        case HandlerType::Pipeline: {
+            auto pipeline = handler.dynamicCast<TypedPipeline>();
+            if (pipeline)
+                pipeline->flush();
+            break;
+        }
+        default:
+            break;
+        }
+    }
+}
+
+} // namespace QtLogger
---
+++ src/qtlogger/typedpipeline.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "filter.h"
+#include "filters/functionfilter.h"
+#include "filters/regexpfilter.h"
+#include "formatter.h"
+#include "formatters/functionformatter.h"
+#include "formatters/patternformatter.h"
+#include "handler.h"
+#include "logger_global.h"
+#include "pipeline.h"
+#include "sink.h"
+
+namespace QtLogger {
+
+class QTLOGGER_EXPORT TypedPipeline : public Pipeline
+{
+public:
+    TypedPipeline();
+
+    void insertAfter(HandlerType type, const HandlerPtr &handler);
+    void insertBetween(HandlerType leftType, HandlerType rightType, const HandlerPtr &handler);
+    void clearType(HandlerType type);
+
+    void appendFilter(const FilterPtr &filter);
+    FunctionFilterPtr appendFilter(const std::function<bool(const LogMessage &)> &function);
+    RegExpFilterPtr appendFilter(const QRegularExpression &regExp);
+    void clearFilters();
+
+    void setFormatter(const FormatterPtr &formatter);
+    FunctionFormatterPtr setFormatter(const std::function<QString(const LogMessage &)> &function);
+    PatternFormatterPtr setFormatter(const QString &pattern);
+    void clearFormatters();
+
+    void appendSink(const SinkPtr &sink);
+    void clearSinks();
+
+    void appendPipeline(const PipelinePtr &pipeline);
+    void clearPipelines();
+
+    void flush();
+};
+
+using TypedPipelinePtr = QSharedPointer<TypedPipeline>;
+
+} // namespace QtLogger
