7825cc86330cbe27ef7853ee4d97ab65bc5d3534
Mikhail Yatsenko <mikhail.yatsenko@gmail.com>
2025-01-07 22:35:56 +0300

    Refactor OwnThreadPipeline to introduce OwnThreadPipelineWorker and enhance event processing

--- src/qtlogger/ownthreadpipeline.h
+++ src/qtlogger/ownthreadpipeline.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <QEvent>
 #include <QPointer>
 #include <QThread>
 
@@ -9,45 +8,7 @@
 
 namespace QtLogger {
 
-QTLOGGER_DECL_SPEC
-static QEvent::Type processLogMessageEventType()
-{
-    static QEvent::Type type = static_cast<QEvent::Type>(QEvent::registerEventType());
-    return type;
-}
-
-struct QTLOGGER_EXPORT ProcessLogMessageEvent : public QEvent
-{
-    LogMessage logMsg;
-    ProcessLogMessageEvent(const LogMessage &logMsg)
-        : QEvent(processLogMessageEventType()), logMsg(logMsg)
-    {
-    }
-};
-
-class QTLOGGER_EXPORT OwnThreadPipelineWorker : public QObject
-{
-    Q_OBJECT
-
-public:
-    explicit OwnThreadPipelineWorker(Handler *handler, QObject *parent = nullptr)
-        : QObject(parent), m_handler(handler)
-    {
-    }
-
-    void customEvent(QEvent *event) override
-    {
-        if (event->type() == processLogMessageEventType()) {
-            auto ev = dynamic_cast<ProcessLogMessageEvent *>(event);
-            if (ev) {
-                dynamic_cast<TypedPipeline *>(m_handler)->process(ev->logMsg);
-            }
-        }
-    }
-
-private:
-    Handler *m_handler;
-};
+class OwnThreadPipelineWorker;
 
 class QTLOGGER_EXPORT OwnThreadPipeline : public TypedPipeline
 {
