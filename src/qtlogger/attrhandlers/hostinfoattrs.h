19541de5e5eb151a72475ff54a68f0112ee0800b
Mikhail Yatsenko <mikhail.yatsenko@gmail.com>
2025-01-08 19:20:03 +0300

    Add HostInfoAttrs class to retrieve host-related attributes

---
+++ src/qtlogger/attrhandlers/hostinfoattrs.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifdef QTLOGGER_NETWORK
+
+#include <QSharedPointer>
+
+#include "../attrhandler.h"
+#include "../logger_global.h"
+
+namespace QtLogger {
+
+class QTLOGGER_EXPORT HostInfoAttrs : public AttrHandler
+{
+public:
+    QVariantHash attributes() const override;
+};
+
+using HostInfoAttrsPtr = QSharedPointer<HostInfoAttrs>;
+
+} // namespace QtLogger
+
+#endif // QTLOGGER_NETWORK
