01998edb00e298f5cbfc571b730f61b8db045ad9
Mikhail Yatsenko <mikhail.yatsenko@gmail.com>
2025-08-04 22:19:28 +0300

    Rename AppInfoAttrs keys to remove underscores
    Map old keys to new keys:
    app_name   -> appname
    app_version -> appversion
    app_dir    -> appdir
    app_path   -> apppath
    
    This changes the public attribute names; update consumers accordingly

--- src/qtlogger/attrhandlers/appinfoattrs.cpp
+++ src/qtlogger/attrhandlers/appinfoattrs.cpp
@@ -8,10 +8,10 @@ QTLOGGER_DECL_SPEC
 AppInfoAttrs::AppInfoAttrs()
 {
     m_attrs = QVariantHash {
-        { QStringLiteral("app_name"), QCoreApplication::applicationName() },
-        { QStringLiteral("app_version"), QCoreApplication::applicationVersion() },
-        { QStringLiteral("app_dir"), QCoreApplication::applicationDirPath() },
-        { QStringLiteral("app_path"), QCoreApplication::applicationFilePath() },
+        { QStringLiteral("appname"), QCoreApplication::applicationName() },
+        { QStringLiteral("appversion"), QCoreApplication::applicationVersion() },
+        { QStringLiteral("appdir"), QCoreApplication::applicationDirPath() },
+        { QStringLiteral("apppath"), QCoreApplication::applicationFilePath() },
         { QStringLiteral("pid"), QCoreApplication::applicationPid() },
     };
 }
