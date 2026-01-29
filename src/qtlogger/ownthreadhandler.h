57bb8fb7ef474039d84adc77212c97e7b3f5e4b1
Mikhail Yatsenko <mikhail.yatsenko@gmail.com>
2026-01-30 02:48:50 +0300

    Refactor OwnThreadHandler thread/worker management
    Change destructor to call resetOwnThread and add ownThread and
    ownThreadIsRunning accessors. Consolidate thread setup so the QThread
    object is always attached to the main thread and aboutToQuit is
    connected. Replace LogMsgEvent with LogEvent (static type()) and post
    LogEvent. Delete the worker via a lambda on thread finish, use QPointer
    for the thread and a raw Worker* for explicit deletion, and simplify
    resetOwnThread to quit and clear thread/worker.

--- src/qtlogger/ownthreadhandler.h
+++ src/qtlogger/ownthreadhandler.h
@@ -28,76 +28,62 @@ public:
         Q_UNUSED(__once)
     }
 
-    ~OwnThreadHandler() override { reset(); }
-
-    OwnThreadHandler<BaseHandler> &moveToOwnThread()
-    {
-        reset();
-
-        if (!m_worker) {
-            m_worker = new Worker(this);
-        }
-
-        if (!m_thread) {
-            m_thread = new QThread();
-
-            if (qApp) {
-                if (qApp->thread() != m_thread->thread()) {
-                    m_thread->moveToThread(qApp->thread());
-                }
-                QObject::connect(qApp, &QCoreApplication::aboutToQuit, m_thread, &QThread::quit);
-            }
-
-            QObject::connect(m_thread, &QThread::finished, m_thread, &QThread::deleteLater);
-            QObject::connect(m_thread, &QThread::finished, m_worker, &QThread::deleteLater);
-
-            m_thread->start();
-        }
-
-        m_worker->moveToThread(m_thread);
-
-        return *this;
-    }
-
-    OwnThreadHandler<BaseHandler> &moveToMainThread()
-    {
-        reset();
-
-        if (!m_worker) {
-            m_worker = new Worker(this);
-        }
-
-        m_worker->moveToThread(qApp->thread());
-
-        return *this;
-    }
-
-    bool ownThreadIsRunning() const { return m_thread && m_thread->isRunning(); }
+    ~OwnThreadHandler() override { resetOwnThread(); }
 
     QThread *ownThread() const { return m_thread; }
 
-    void reset()
+    bool ownThreadIsRunning() const { return m_thread && m_thread->isRunning(); }
+
+    OwnThreadHandler<BaseHandler> &moveToOwnThread()
     {
-        if (m_thread) {
-            m_thread->quit();
-            m_thread->wait(100);
-            if (m_thread && m_thread->isRunning()) {
-                m_thread->terminate();
+        if (m_thread)
+            return *this;
+
+        m_thread = new QThread();
+
+        if (qApp) {
+            // The thread object must be guaranteed to be attached to the main thread regardless of
+            // where this method was called from
+            if (qApp->thread() != m_thread->thread()) {
+                m_thread->moveToThread(qApp->thread());
             }
-            if (m_thread) {
-                delete m_thread;
-            }
+            QObject::connect(qApp, &QCoreApplication::aboutToQuit, m_thread, &QThread::quit);
         }
 
-        if (m_worker) {
-            delete m_worker;
-        }
+        QObject::connect(m_thread, &QThread::finished, m_thread, &QThread::deleteLater);
+
+        m_worker = new Worker(/* handler (not parent!) */ this);
+        m_worker->moveToThread(m_thread);
+
+        // The deletion of the worker occurs only in this place after the thread stops, so we can
+        // keep a pointer to it via closures
+        const auto worker = m_worker;
+        QObject::connect(m_thread, &QThread::finished, [worker]() {
+            // We cannot delete worker via deleteLater because m_thread will not process incoming
+            // events anymore
+            delete worker;
+        });
+
+        m_thread->start();
+
+        return *this;
+    }
+
+    void resetOwnThread()
+    {
+        if (!m_thread)
+            return;
+
+        m_thread->quit();
+        m_thread.clear();
+
+        m_worker = nullptr;
     }
 
     bool process(LogMessage &lmsg) override
     {
         if (m_worker) {
-            QCoreApplication::postEvent(m_worker, new LogMsgEvent(lmsg));
+            QCoreApplication::postEvent(m_worker, new LogEvent(lmsg));
         } else {
             BaseHandler::process(lmsg);
         }
@@ -105,16 +91,17 @@ public:
     }
 
 private:
-    static QEvent::Type getLogMsgEventType()
+    struct LogEvent : public QEvent
     {
-        static QEvent::Type type = static_cast<QEvent::Type>(QEvent::registerEventType());
-        return type;
-    }
+        LogEvent(const LogMessage &lmsg) : QEvent(type()), lmsg(lmsg) { }
 
-    struct LogMsgEvent : public QEvent
-    {
+        static QEvent::Type type()
+        {
+            static QEvent::Type _type = static_cast<QEvent::Type>(QEvent::registerEventType());
+            return _type;
+        }
+
         LogMessage lmsg;
-        LogMsgEvent(const LogMessage &lmsg) : QEvent(getLogMsgEventType()), lmsg(lmsg) { }
     };
 
     class Worker : public QObject
@@ -124,10 +111,10 @@ private:
 
         void customEvent(QEvent *event) override
         {
-            if (event->type() == getLogMsgEventType()) {
-                auto lmEvent = dynamic_cast<LogMsgEvent *>(event);
-                if (lmEvent) {
-                    m_handler->BaseHandler::process(lmEvent->lmsg);
+            if (event->type() == LogEvent::type()) {
+                auto logEvent = dynamic_cast<LogEvent *>(event);
+                if (logEvent) {
+                    m_handler->BaseHandler::process(logEvent->lmsg);
                 }
             }
         }
@@ -136,8 +123,9 @@ private:
         OwnThreadHandler<BaseHandler> *m_handler;
     };
 
-    QPointer<Worker> m_worker;
+private:
     QPointer<QThread> m_thread;
+    Worker *m_worker = nullptr;
 };
 
 } // namespace QtLogger
