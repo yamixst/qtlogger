2f4cbee02975f37cc51a0ad88060b3b88786183d
Mikhail Yatsenko <mikhail.yatsenko@gmail.com>
2026-02-13 22:30:13 +0300

    Add App UUID to logger setup

--- examples/simple_config/main.cpp
+++ examples/simple_config/main.cpp
@@ -31,6 +31,7 @@ int main(int argc, char *argv[])
 
     gQtLogger
         .addAppInfo()
+        .addAppUuid()
         .pipeline()
             .addSeqNumber()
             .filter("^(?!.*password|.*secret).*$")
